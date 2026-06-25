1c2ea63ab484db5b55e65d886b7f3df356699cc0
msamatdev <mathis.samat@gmail.com>
2026-06-25 14:41:35 +0200

    feat: add button to invert currencies

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
+
 st.title("Convertisseur de devises")
+
 
 rates = {
     "EUR": 1,
@@ -8,9 +10,39 @@ rates = {
     "JPY": 130
 }
 
+
+# Currencies select value initialization
+if "from_currency" not in st.session_state:
+    st.session_state.from_currency = "EUR"
+
+
+if "to_currency" not in st.session_state:
+    st.session_state.to_currency = "USD"
+
+
 amount = st.number_input("Montant :", min_value=0.0, format="%.2f")
-from_currency = st.selectbox("De :", rates.keys())
-to_currency = st.selectbox("Vers :", rates.keys())
+
+
+from_currency = st.selectbox(
+    "De :",
+    list(rates.keys()),
+    key="from_currency"
+)
+
+
+to_currency = st.selectbox(
+    "Vers :",
+    list(rates.keys()),
+    key="to_currency"
+)
+
+
+def invert_currencies():
+    """
+    Callback function that invert the two selected currencies
+    """
+    st.session_state.from_currency, st.session_state.to_currency = st.session_state.to_currency, st.session_state.from_currency,
+
 
 if st.button("Convertir"):
     if amount > 0:
@@ -21,3 +53,9 @@ if st.button("Convertir"):
             st.error("Les devises doivent être différentes")
     else:
         st.error("Le montant doit être superieur a 0")
+
+
+st.button(
+    "Inverser les devises",
+    on_click=invert_currencies
+)
